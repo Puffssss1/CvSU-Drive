4e70308a84181e64b1868193bddbe4b856a5e2dc
Puffssss1 <budjiangelo@gmail.com>
2024-09-11 21:49:05 +0800

    material ui initial test

---
+++ app/components/LoginBtn.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
+function LoginBtn() {
+  return (
+    <div>
+      Material Ui Test
+      <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+    </div>
+  )
+}
+
+export default LoginBtn
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
+import  LoginBtn from './components/LoginBtn'
 
 export default function Home() {
   return (
     <div>
       Home Page
-      Material Ui Test
-      <Stack spacing={2} direction="row">
-        <Button variant="text">Text</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="outlined">Outlined</Button>
-      </Stack>
+      <LoginBtn />
     </div>
   );
 }
