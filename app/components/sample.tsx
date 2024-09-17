1904fce2062b0934533c34242b0b4faa5c1b574e
Puffssss1 <budjiangelo@gmail.com>
2024-09-17 23:24:21 +0800

    mock data

--- app/components/sample.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function KUPAL() {
-  return (
-    <div>sarap 6k</div>
-  )
-}
-
-export default KUPAL
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import KUPAL from '../components/sample'
 
 
 function Profile() {
   return (
     <div>
-      <KUPAL />
+      Profile
     </div>
   )
 }
