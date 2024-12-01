3cb156446e7e92ceef525f612753a7167fdf2ad8
Keezgeez <135505653+Keezgeez@users.noreply.github.com>
2024-12-01 21:46:01 +0800

    Update Header
    Icon Responsive to Profile page

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,20 +15,22 @@ function Header() {
         <div className='flex flex-row w-full justify-between gap-40 p-5 align-middle'>
             <Sidebar/>
             <h1 className="text-fontGreen text-center text-2xl">Welcome to CvSU Drive</h1>
-            <div className='flex flex-row gap-1 align-middle'>
-              <ul className='text-right text-sm w-24 m-0 p-0 gap-0'>
-                  {session?.user ? (
-                  <>
-                    <li>{session.user.name},</li>
-                    <li>{session?.role}</li>
-                  </>
-                ) : (
-                  <li>User not logged in</li>
-                )}
-              </ul>
-              <HiOutlineUserCircle className='size-10'></HiOutlineUserCircle>
-              <LogoutBtn/>
-            </div>
+            <a href="/profile">
+              <div className='flex flex-row gap-1 align-middle'>
+                <ul className='text-right text-sm w-24 m-0 p-0 gap-0'>
+                    {session?.user ? (
+                    <>
+                      <li>{session.user.name},</li>
+                      <li>{session?.role}</li>
+                    </>
+                  ) : (
+                    <li>User not logged in</li>
+                  )}
+                </ul>
+                <HiOutlineUserCircle className='size-10'></HiOutlineUserCircle>
+                <LogoutBtn/>
+              </div>
+            </a>
             
         </div>
     </div>
